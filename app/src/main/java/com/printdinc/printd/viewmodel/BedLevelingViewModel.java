8b83a0f23945ea81095483a59f5b89214b7d041a
emilykimhuynh <emilyhuynh101@gmail.com>
2017-04-03 23:01:50 -0500

    created bed leveling activity and viewmodel

---
+++ app/src/main/java/com/printdinc/printd/viewmodel/BedLevelingViewModel.java
@@ -0,0 +1,21 @@
+package com.printdinc.printd.viewmodel;
+
+
+import android.content.Context;
+import android.databinding.BaseObservable;
+
+/**
+ * Created by Emily on 4/3/17.
+ */
+
+public class BedLevelingViewModel extends BaseObservable implements ViewModel {
+
+    private static final String TAG = "BedLevelingViewModel";
+
+    private Context context;
+
+    public void destroy() {
+
+    }
+
+}
