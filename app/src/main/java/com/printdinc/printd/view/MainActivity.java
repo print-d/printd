a7fc4ef127bfb5fcdf96f63a7e7635be85f285e9
emilykimhuynh <emilyhuynh101@gmail.com>
2017-04-05 16:01:34 -0500

    final changes for demo tomorrow

--- app/src/main/java/com/printdinc/printd/view/MainActivity.java
+++ app/src/main/java/com/printdinc/printd/view/MainActivity.java
@@ -5,6 +5,8 @@ import android.databinding.DataBindingUtil;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.View;
 
 import com.printdinc.printd.R;
 import com.printdinc.printd.databinding.ActivityMainBinding;
@@ -26,6 +28,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
     protected void onResume() {
         super.onResume();
 
@@ -42,6 +50,16 @@ public class MainActivity extends AppCompatActivity {
         String scheme = intent.getScheme();
 
         mainViewModel.thingiverseLogin(uri);
+    }
+
+    public void goToThingiverse (View view) {
+        goToUrl ( "http://thingiverse.com/");
+    }
+
+    private void goToUrl (String url) {
+        Uri uriUrl = Uri.parse(url);
+        Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
+        startActivity(launchBrowser);
     }
 
     @Override
