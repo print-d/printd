ce974f623c8388c79528194571fe27754d5b7d90
Adam Tremonte <abtremonte@crimson.ua.edu>
2017-04-11 12:47:03 -0500

    Merging

--- app/src/main/java/com/printdinc/printd/service/OctoprintService.java
+++ app/src/main/java/com/printdinc/printd/service/OctoprintService.java
@@ -1,12 +1,15 @@
 package com.printdinc.printd.service;
 
 import com.printdinc.printd.model.PrintHeadCommand;
+import com.printdinc.printd.model.ConnectionState;
+import com.printdinc.printd.model.SimpleCommand;
 import com.printdinc.printd.model.SliceCommand;
 
 import okhttp3.MultipartBody;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.GET;
 import retrofit2.http.Multipart;
 import retrofit2.http.POST;
 import retrofit2.http.Part;
@@ -14,6 +17,12 @@ import retrofit2.http.Path;
 import rx.Observable;
 
 public interface OctoprintService {
+
+    @GET("api/connection")
+    Observable<ConnectionState> getConnectionState();
+
+    @POST("api/connection")
+    Observable<ResponseBody> connectCommand(@Body SimpleCommand command);
 
     @Multipart
     @POST("api/files/{location}")
